d8d484cdc7c82a4abb083c605e9c8bc7d7048717
alfred <aababio75@gmail.com>
2016-01-24 11:14:46 -0500

    I wish git commits had /shrgu

--- receiver/receiver.js
+++ receiver/receiver.js
@@ -96,11 +96,11 @@ window.onload = function() {
         chooseCard( messageData[ 1 ] );
         window.messageBus.send( event.senderId, 'PICK_CARD_SUCCESS' );
       break;
-      case 'HIGHER_OR_LOWER': // higher or lower from row 
+      case 'HIGHER_OR_LOWER': // higher or lower from row
         if (higherOrLower( messageData[ 1 ] )) {
           window.messageBus.send( event.senderId, 'HIGH_LOW_SUCCESS' ); // ??????
         } else {
-          window.messageBus.send( event.senderId, 'FAILURE' ); // ??????
+          sendLoseMessage( event.senderId ); // ??????
           changeTurn();
         }
       break;
@@ -108,7 +108,7 @@ window.onload = function() {
         if ( insideOrOutside( messageData[ 1 ] ) ) {
           window.messageBus.send( event.senderId, 'INSIDE_OUTSIDE_SUCCESS' ); // ??????
         } else {
-          window.messageBus.send( event.senderId, 'FAILURE' ); // ??????
+          sendLoseMessage( event.senderId ); // ??????
           changeTurn();
         }
       break;
@@ -116,7 +116,7 @@ window.onload = function() {
         if ( smokeOrFire( messageData[ 1 ] ) ) {
           window.messageBus.send( event.senderId, 'SMOKE_OR_FIRE_SUCCESS' ); // ??????
         } else {
-          window.messageBus.send( event.senderId, 'FAILURE' ); // ??????
+          sendLoseMessage( event.senderId ); // ??????
           changeTurn();
         }
       break;
@@ -327,4 +327,9 @@ function sendStartGame( senderId ) {
 
 function sendNameAck( senderId ) {
   window.messageBus.send( senderId, 'NAME_RECEIVED' );
+}
+
+function sendLoseMessage ( senderId ) {
+  showScreen('failure');
+  window.messageBus.send( event.senderId, 'FAILURE' ); // ??????
 }
